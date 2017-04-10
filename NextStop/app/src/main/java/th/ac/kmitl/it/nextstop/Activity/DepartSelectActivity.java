3fb437c289bda2f8e9a2450e7239d367a39ed9c6
Trithep Ratanapipob <the_prince_of_nightmare@hotmail.co.jp>
2017-04-11 06:57:21 +0700

    get userlocation

--- NextStop/app/src/main/java/th/ac/kmitl/it/nextstop/Activity/DepartSelectActivity.java
+++ NextStop/app/src/main/java/th/ac/kmitl/it/nextstop/Activity/DepartSelectActivity.java
@@ -1,15 +1,20 @@
 package th.ac.kmitl.it.nextstop.Activity;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import th.ac.kmitl.it.nextstop.R;
+import th.ac.kmitl.it.nextstop.databinding.ActivityDepartSelectBinding;
+import th.ac.kmitl.it.nextstop.databinding.ActivityMainBinding;
 
 public class DepartSelectActivity extends AppCompatActivity {
+
+    ActivityDepartSelectBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_depart_select);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_landing);
     }
 }
--- NextStop/app/src/main/java/th/ac/kmitl/it/nextstop/Activity/LandingActivity.java
+++ NextStop/app/src/main/java/th/ac/kmitl/it/nextstop/Activity/LandingActivity.java
@@ -1,19 +1,73 @@
 package th.ac.kmitl.it.nextstop.Activity;
 
 import android.databinding.DataBindingUtil;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
 import th.ac.kmitl.it.nextstop.R;
+import th.ac.kmitl.it.nextstop.databinding.ActivityLandingBinding;
 import th.ac.kmitl.it.nextstop.databinding.ActivityMainBinding;
 
-public class LandingActivity extends AppCompatActivity {
+public class LandingActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener{
 
-    ActivityMainBinding binding;
+    ActivityLandingBinding binding;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_landing);
+        binding.tvHello.setText("HHHHHHHHHHHH\nHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH");
+        binding.la.setText("Waiting get location");
+        binding.lo.setText("Waiting get location");
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+    }
+
+    @Override
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            binding.la.setText(String.valueOf(mLastLocation.getLatitude()));
+            binding.lo.setText(String.valueOf(mLastLocation.getLongitude()));
+        }else{
+            binding.la.setText("Cannot get location");
+            binding.lo.setText("Cannot get location");
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
 }
