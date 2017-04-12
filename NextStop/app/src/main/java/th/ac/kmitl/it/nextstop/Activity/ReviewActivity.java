7a96ef98c06b83c561d0a2521bb558a5c7c31a80
Trithep Ratanapipob <the_prince_of_nightmare@hotmail.co.jp>
2017-04-12 20:14:00 +0700

    Add listener Button Review

--- NextStop/app/src/main/java/th/ac/kmitl/it/nextstop/Activity/ReviewActivity.java
+++ NextStop/app/src/main/java/th/ac/kmitl/it/nextstop/Activity/ReviewActivity.java
@@ -1,15 +1,68 @@
 package th.ac.kmitl.it.nextstop.Activity;
 
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import th.ac.kmitl.it.nextstop.Model.Station;
 import th.ac.kmitl.it.nextstop.R;
+import th.ac.kmitl.it.nextstop.databinding.ActivityReviewBinding;
 
 public class ReviewActivity extends AppCompatActivity {
+
+    ActivityReviewBinding binding;
+    String departStation;
+    String desStation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_review);
+        initInstance();
+    }
+
+    private void initInstance(){
+        Intent intent = getIntent();
+        departStation = intent.getStringExtra("departStation");
+        desStation = intent.getStringExtra("desStation");
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_review);
+        binding.departButton.setText(departStation + " ▼");
+        binding.departButton.setOnClickListener(listener);
+        binding.startButton.setOnClickListener(listener);
+        binding.nameStation.setText(desStation);
+
+        binding.backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
+    }
+
+    View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if (binding.departButton == view) {
+                Intent intent = new Intent(ReviewActivity.this, DepartSelectActivity.class);
+                startActivityForResult(intent, 1);
+            } else if (binding.startButton == view) {
+                Intent intent = new Intent(ReviewActivity.this, DestinationActivity.class);
+                intent.putExtra("departStation", departStation);
+                startActivity(intent);
+            }
+        }
+    };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                binding.departButton.setText(data.getStringExtra("station") + " ▼");
+            }
+        }
     }
 }
