422852bd12d2d3a78dccb2fb637f24ca0e32b069
Trithep Ratanapipob <the_prince_of_nightmare@hotmail.co.jp>
2017-04-12 18:01:46 +0700

    add Review Activity

--- NextStop/app/src/main/java/th/ac/kmitl/it/nextstop/Activity/DestinationActivity.java
+++ NextStop/app/src/main/java/th/ac/kmitl/it/nextstop/Activity/DestinationActivity.java
@@ -1,8 +1,11 @@
 package th.ac.kmitl.it.nextstop.Activity;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 
 import th.ac.kmitl.it.nextstop.Model.StationList;
 import th.ac.kmitl.it.nextstop.R;
@@ -16,6 +19,23 @@ public class DestinationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_destination);
         binding.setViewModel(StationList.getStations());
+
+        binding.stationList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                if(true){
+                    Intent intent = new Intent(DestinationActivity.this, MainActivity.class);
+                    intent.putExtra("station", StationList.getStations().getStationFormIndex(i).getName());
+                    startActivity(intent);
+                }else {
+                    Intent data = new Intent();
+                    data.putExtra("station", StationList.getStations().getStationFormIndex(i).getName());
+                    setResult(RESULT_OK, data);
+                    finish();
+                }
+            }
+        });
     }
 
 
