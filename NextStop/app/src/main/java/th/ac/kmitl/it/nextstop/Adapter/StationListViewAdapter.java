f4fbc312c4bea21c3ead33d5042896876f9eb8bf
Trithep Ratanapipob <the_prince_of_nightmare@hotmail.co.jp>
2017-05-06 12:53:12 +0700

    adapter binding

--- NextStop/app/src/main/java/th/ac/kmitl/it/nextstop/Adapter/StationListViewAdapter.java
+++ NextStop/app/src/main/java/th/ac/kmitl/it/nextstop/Adapter/StationListViewAdapter.java
@@ -1,9 +1,13 @@
 package th.ac.kmitl.it.nextstop.Adapter;
 
+import android.databinding.BindingAdapter;
 import android.databinding.ViewDataBinding;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import me.tatarka.bindingcollectionadapter2.BindingListViewAdapter;
 import th.ac.kmitl.it.nextstop.Model.Station;
@@ -33,11 +37,33 @@ public class StationListViewAdapter extends BindingListViewAdapter<Station> {
     public void onBindBinding(ViewDataBinding binding, int variableId, int layoutRes, int position, Station item) {
 
         item.setConnectionLabel("จุดเชื่อมต่อ รถไฟฟ้า " + item.getConnection());
-        item.setConnectionIcon("iconmrt");
+        item.setConnectionIcon("drawable/iconmrt");
         super.onBindBinding(binding, variableId, layoutRes, position, item);
 
         Log.e("onBindBinding", "variableId: " + variableId + "  layoutRes: " + layoutRes + " at position: " + position);
     }
 
+    @BindingAdapter("android:src")
+    public static void setImageUri(ImageView view, String imageUri) {
+        if (imageUri == null) {
+            view.setImageURI(null);
+        } else {
+            view.setImageURI(Uri.parse(imageUri));
+        }
+    }
 
+    @BindingAdapter("android:src")
+    public static void setImageUri(ImageView view, Uri imageUri) {
+        view.setImageURI(imageUri);
+    }
+
+    @BindingAdapter("android:src")
+    public static void setImageDrawable(ImageView view, Drawable drawable) {
+        view.setImageDrawable(drawable);
+    }
+
+    @BindingAdapter("android:src")
+    public static void setImageResource(ImageView imageView, int resource) {
+        imageView.setImageResource(resource);
+    }
 }
