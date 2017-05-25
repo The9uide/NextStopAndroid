cd1827a68bee4f5e8a8df37068d0222e50644fb5
Trithep Ratanapipob <the_prince_of_nightmare@hotmail.co.jp>
2017-05-25 17:51:40 +0700

    add fragment

---
+++ NextStop/app/src/main/java/th/ac/kmitl/it/nextstop/Fragment/ShopFragment.java
@@ -0,0 +1,30 @@
+package th.ac.kmitl.it.nextstop.Fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by The9uide on 13-May-17.
+ */
+
+public class ShopFragment extends Fragment {
+    public ShopFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
+}
